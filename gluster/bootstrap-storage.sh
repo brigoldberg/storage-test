5a99f7acd562c70793c9f8c535d40f1b7764b3c9
Brian Goldberg <brian@dev-ny02-wks002.local>
2018-03-16 17:50:11 -0400

    gluster install script done

--- gluster/bootstrap-storage.sh
+++ gluster/bootstrap-storage.sh
@@ -10,5 +10,27 @@ yum -y install htop vim tree wget
 
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 
-cd /vagrant
+echo "storage-1     192.168.33.11" >> /etc/hosts     
+echo "storage-2     192.168.33.12" >> /etc/hosts     
 
+# Storage disk tasks
+sfdisk /dev/sdb << EOF
+;
+EOF
+mkfs.xfs -i size=512 /dev/sdb1
+mkdir /data/brick1
+echo '/dev/sdb1 /data/brick1 xfs defaults 1 2' >> /etc/fstab
+mount -a && mount
+
+# GlusterFS installation
+yum -y install glusterfs-server
+if [[ ${HOSTNAME} == "storage-1" ]]; then
+    gluster peer probe storage-2
+else
+    gluster peer probe storage-1
+fi
+
+mkdir -p /data/brick1/gv0
+
+gluster volume create gv0 replica 2 storage-1:/data/brick1/gv0 storage-2:/data/brick1/gv0
+gluster volume start gv0
